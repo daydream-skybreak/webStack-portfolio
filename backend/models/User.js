c401782d4bd33432ead85bf76a4bb1e0f496842f
Samuel <samueldesta323@gmail.com>
2023-04-11 15:29:53 +0300

    Added the user model;

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import sha1 from 'sha1';
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+})
+
+export default mongoose.model('User', userSchema);
