29df690f8cc56394309155db1d2cfc75dae276c7
daydream-skybreak <93473954+daydream-skybreak@users.noreply.github.com>
2023-04-14 15:48:00 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use('/api/users', router)
 
 mongoose
-    .connect('mongodb+srv://Adonay:RdtKvgiSZ10VVqQF@portfolio-x.lpwhmwr.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect('mongodb+srv://<username>:<password>@portfolio-x.lpwhmwr.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => app.listen(5000))
     .then(() => console.log('Connected to mongoDB and listening port 5000'))
     .catch((err) => console.error(`Error: ${err}`))
