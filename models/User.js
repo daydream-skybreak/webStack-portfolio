e6f5fe339e7faabcf3c0cdf11b1c496605000f70
Mulugeta Achiso <anem1351@gmail.com>
2023-04-18 02:14:19 -0700

    models

--- models/User.js
+++ models/User.js
@@ -16,7 +16,8 @@ const userSchema = new Schema({
         type: String,
         required: true,
         minlength: 8
-    }
-})
+    },
+    blogs: [{type: mongoose.Types.ObjectId, ref: "blog", required: true}],
+});
 
 export default mongoose.model('User', userSchema);
