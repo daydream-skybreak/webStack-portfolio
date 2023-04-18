805ac275eb9e045ae8a67bda3128b8ba0909d7e6
Mulugeta Achiso <anem1351@gmail.com>
2023-04-18 02:11:34 -0700

    user routes

--- routes/blogRouters.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllBlogs } from "../controllers/blogController";
-const blogRouter = express.Router();
-
-blogRouter.get("/", getAllBlogs);
-
-export default blogRouter;
