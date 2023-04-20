c8548f2a755788d4e140aa8cd8fc7ed83c8bf078
Samuel <samueldesta323@gmail.com>
2023-04-20 15:02:26 +0300

    started using environmental variables

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import mongoose from "mongoose";
 import router from './routes/userRouter'
+import blogRouter from "./routes/blogRouter";
 
 const app = express();
 app.use(express.json())
 app.use('/api/users', router)
-
+app.use('/api/blogs', blogRouter)
+/**
+ * Environmental variables
+ * username - mongodb atlas username
+ * password - password to the mongo atlas database
+ */
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
 mongoose
-    .connect('mongodb+srv://<username>:<password>@portfolio-x.lpwhmwr.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect(`mongodb+srv://${username}:${password}@portfolio-x.lpwhmwr.mongodb.net/blog?retryWrites=true&w=majority`)
     .then(() => app.listen(5000))
-    .then(() => console.log('Connected to mongoDB and listening port 5000'))
+    .then(() =>  console.log('Connected to mongoDB and listening port 5000'))
     .catch((err) => console.error(`Error: ${err}`))
 
